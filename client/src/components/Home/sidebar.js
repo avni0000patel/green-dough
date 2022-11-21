b68a3d6bb60c70e8d30d39dd64a57543a1417391
Avni <avni0000patel@gmail.com>
2022-11-20 21:43:46 -0500

    working links for sidebar

--- client/src/components/Home/sidebar.js
+++ client/src/components/Home/sidebar.js
@@ -2,28 +2,41 @@ import React, { useState } from "react";
 import { Link } from 'react-router-dom';
 import { ProSidebar, Menu, MenuItem, SidebarHeader, SidebarFooter, SidebarContent } from "react-pro-sidebar";
 import { FiHome, FiLogOut, FiArrowLeftCircle, FiArrowRightCircle } from "react-icons/fi";
-import { BiCog } from "react-icons/bi";
-import { SiApacheairflow } from "react-icons/si";
-import { GiAbstract050 } from "react-icons/gi";
-import Auth from "../../utils/auth";
+// import { BiCog } from "react-icons/bi";
+// import { SiApacheairflow } from "react-icons/si";
+// import { GiAbstract050 } from "react-icons/gi";
+// import Auth from "../../utils/auth";
 import "react-pro-sidebar/dist/css/styles.css";
 import './sidebar.css';
 
-export default function Sidenav() {
+function Sidebar() {
+
     const [menuCollapse, setMenuCollapse] = useState(false);
+    const [activeIndex, setActiveIndex] = useState(() => {
+        const initialIndex =
+            window.location.pathname === '/' ? 0
+                : window.location.pathname === '/login' ? 1
+                    : window.location.pathname === 'signup' ? 2
+                        : 0;
+        return initialIndex;
+    });
+
     const menuIconClick = () => {
-        menuCollapse ? setMenuCollapse(false) : setMenuCollapse(true);
+        setMenuCollapse(!menuCollapse);
     };
 
     return (
         <>
             <div id="header">
+                {/* collapsed props to change menu size using menucollapse state */}
                 <ProSidebar collapsed={menuCollapse}>
                     <SidebarHeader>
                         <div className="logotext">
-                            <p>{menuCollapse ? <GiAbstract050 /> : <SiApacheairflow />}</p>
+                            {/* small and big change using menucollapse state */}
+                            <p>{menuCollapse ? "GD" : "Green Dough"}</p>
                         </div>
                         <div className="closemenu" onClick={menuIconClick}>
+                            {/* changing menu collapse icon on click */}
                             {menuCollapse ? (
                                 <FiArrowRightCircle />
                             ) : (
@@ -31,39 +44,31 @@ export default function Sidenav() {
                             )}
                         </div>
                     </SidebarHeader>
-                    <Menu iconShape="square">
-                        {Auth.loggedIn() ? (
-                            <>
-                                <SidebarContent>
-                                    <Link to='/' style={{ textDecoration: 'none' }}>
-                                        <MenuItem active={true} icon={<FiHome />}>Home</MenuItem>
-                                    </Link>
-                                    <MenuItem icon={<BiCog />}>Settings</MenuItem>
-                                </SidebarContent>
-                                <SidebarFooter>
-                                    <Menu iconShape="square">
-                                        <MenuItem icon={<FiLogOut />}>Logout</MenuItem>
-                                    </Menu>
-                                </SidebarFooter>
-                            </>
-                        ) : (
-                            <>
-                                <SidebarContent>
-                                    <Link to='/' style={{ textDecoration: 'none' }}>
-                                        <MenuItem active={true} icon={<FiHome />}>Home</MenuItem>
-                                    </Link>
-                                    <Link to='/login' style={{ textDecoration: 'none' }}>
-                                        <MenuItem icon={<FiHome />}>Login</MenuItem>
-                                    </Link>
-                                    <Link to='/signup' style={{ textDecoration: 'none' }}>
-                                        <MenuItem icon={<FiHome />}>Signup</MenuItem>
-                                    </Link>
-                                </SidebarContent>
-                            </>
-                        )}
-                    </Menu>
+                    <SidebarContent>
+                        <Menu iconShape="square">
+                            <MenuItem active={activeIndex === 0} icon={<FiHome />} >
+                                Home
+                                <Link id="MenuItemHome" to="/" onClick={() => setActiveIndex(0)} />
+                            </MenuItem>
+                            <MenuItem active={activeIndex === 1} icon={<FiHome />} >
+                                Login
+                                <Link id="MenuItemLogin" to="/login" onClick={() => setActiveIndex(1)} />
+                            </MenuItem>
+                            <MenuItem active={activeIndex === 2} icon={<FiHome />}>
+                                Signup
+                                <Link id="MenuItemSignup" to="/signup" onClick={() => setActiveIndex(2)} />
+                            </MenuItem>
+                        </Menu>
+                    </SidebarContent>
+                    <SidebarFooter>
+                        <Menu iconShape="square">
+                            <MenuItem icon={<FiLogOut />}>Logout</MenuItem>
+                        </Menu>
+                    </SidebarFooter>
                 </ProSidebar>
             </div>
         </>
     );
 }
+
+export default Sidebar;
