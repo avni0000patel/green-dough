ab195a525f4b0077e80fd189de768a9d013b9c24
Avni <avni0000patel@gmail.com>
2022-12-07 16:20:39 -0500

    profiles in user model

--- server/models/User.js
+++ server/models/User.js
@@ -19,6 +19,12 @@ const userSchema = new Schema({
         required: true,
         minlength: 5,
     },
+    profiles: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Profile',
+        },
+    ],
 });
 
 userSchema.pre('save', async function (next) {
