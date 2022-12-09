8fd1e28ae524178122b53b5fa89a88fe2afd8dab
Avni <avni0000patel@gmail.com>
2022-12-08 20:38:11 -0500

    only upload the most recent image

--- client/src/components/Home/profileList.js
+++ client/src/components/Home/profileList.js
@@ -4,10 +4,12 @@ const PostList = ({
     profiles,
 }) => {
 
+    let lastElement = profiles.slice(-1);
+    console.log(lastElement);
     return (
         <div>
             {profiles &&
-                profiles.map((profile) => (
+                profiles.slice(-1).map((profile) => (
                     <div key={profile._id}>
                         <div>
                             <img className="profile__image" src={profile.image} alt="profileimage" width={80} height={80}></img>
